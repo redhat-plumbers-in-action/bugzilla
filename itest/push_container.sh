c63e7421b56a32486a965130b9330dff4712a033
Jan Macku <jamacku@redhat.com>
2022-09-16 16:12:30 +0200

    Update links after fork
    `Mossop/bugzilla-ts` -> `redhat-plumbers-in-action/bugzilla`

--- itest/push_container.sh
+++ itest/push_container.sh
@@ -4,5 +4,5 @@ set -e
 
 ITEST=$(cd $(dirname "${BASH_SOURCE[0]:-$0}") && pwd | sed -e s/\\/$//g)
 
-docker build --platform linux/amd64 -t ghcr.io/mossop/bugzilla-ts-test:latest "${ITEST}/container"
-docker push ghcr.io/mossop/bugzilla-ts-test:latest
+docker build --platform linux/amd64 -t ghcr.io/redhat-plumbers-in-action/bugzilla-test:latest "${ITEST}/container"
+docker push ghcr.io/redhat-plumbers-in-action/bugzilla-test:latest
