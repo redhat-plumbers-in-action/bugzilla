510c36c497e65376109d58afd40451d838382f57
Jan Macku <jamacku@redhat.com>
2022-09-19 09:49:16 +0200

    Update links to integration container

--- itest/start_container.sh
+++ itest/start_container.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-CONTAINER=$(docker ps -aq -f name=^bugzilla-ts-test$)
+CONTAINER=$(docker ps -aq -f name=^integration$)
 if [ "${CONTAINER}" ]; then
   echo "Container already started."
   exit 0
@@ -11,7 +11,7 @@ fi
 ITEST=$(cd $(dirname "${BASH_SOURCE[0]:-$0}") && pwd | sed -e s/\\/$//g)
 rm -rf "${ITEST}/db"
 mkdir -p "${ITEST}/db"
-docker run --rm -d -p 8088:80 --name bugzilla-ts-test ghcr.io/redhat-plumbers-in-action/bugzilla-test:latest >/dev/null
+docker run --rm -d -p 8088:80 --name integration ghcr.io/redhat-plumbers-in-action/bugzilla/integration:latest >/dev/null
 
 echo "Waiting for http service to start..."
 while ! curl http://localhost:8088/ >/dev/null 2>/dev/null
