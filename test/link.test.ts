a7b4177d23ac57786b77d64ab172279662175747
Jan Macku <jamacku@redhat.com>
2024-05-10 14:25:20 +0200

    deps: update dependencies

--- test/link.test.ts
+++ test/link.test.ts
@@ -1,13 +1,7 @@
-import {
-  DefaultBodyType,
-  PathParams,
-  ResponseComposition,
-  rest,
-  RestContext,
-  RestRequest,
-} from 'msw';
-import { setupServer } from 'msw/node';
 import { URL } from 'url';
+
+import { http, HttpResponse } from 'msw';
+import { setupServer } from 'msw/node';
 
 import { PublicLink, ApiKeyLink, PasswordLink } from '../src/link';
 import { int, object, string } from '../src/validators';
@@ -18,31 +12,13 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-function callArgs<A extends any[]>(
-  fn: jest.Mock<unknown, A>,
-  call: number = 0,
-): A {
-  let args = fn.mock.calls[call];
-  if (args === undefined) {
-    throw new Error(
-      'Mock function was not called the expected number of times.',
-    );
-  }
-
-  return args;
-}
-
 test('PublicLink', async () => {
   let link = new PublicLink(new URL('http://bugzilla.test.org/test/'));
 
   let responseHandler = jest.fn(
-    (
-      req: RestRequest<never, PathParams>,
-      res: ResponseComposition<DefaultBodyType>,
-      ctx: RestContext,
-    ) =>
-      res(
-        ctx.json({
+    () =>
+      new HttpResponse(
+        JSON.stringify({
           foo: 'Bar',
           length: 38,
         }),
@@ -50,7 +26,7 @@ test('PublicLink', async () => {
   );
 
   server.use(
-    rest.get('http://bugzilla.test.org/test/rest/foo', responseHandler),
+    http.get('http://bugzilla.test.org/test/rest/foo', responseHandler),
   );
 
   let testSpec = object({
@@ -66,10 +42,14 @@ test('PublicLink', async () => {
   });
 
   expect(responseHandler).toHaveBeenCalledTimes(1);
-  let [{ url, method, headers }] = callArgs(responseHandler);
-  expect(method).toBe('GET');
-  expect(url.href).toBe('http://bugzilla.test.org/test/rest/foo');
-  expect(headers.get('Accept')).toBe('application/json');
+  expect(responseHandler).toHaveBeenCalledWith(
+    expect.objectContaining({
+      request: expect.objectContaining({
+        method: 'GET',
+        url: 'http://bugzilla.test.org/test/rest/foo',
+      }),
+    }),
+  );
 });
 
 test('ApiKeyLink', async () => {
@@ -78,17 +58,18 @@ test('ApiKeyLink', async () => {
     'my-api-key',
   );
 
-  let responseHandler = jest.fn((req, res, ctx) =>
-    res(
-      ctx.json({
-        foo: 'Bar',
-        length: 38,
-      }),
-    ),
+  let responseHandler = jest.fn(
+    () =>
+      new HttpResponse(
+        JSON.stringify({
+          foo: 'Bar',
+          length: 38,
+        }),
+      ),
   );
 
   server.use(
-    rest.get('http://bugzilla.test.org/test/rest/foo', responseHandler),
+    http.get('http://bugzilla.test.org/test/rest/foo', responseHandler),
   );
 
   let testSpec = object({
@@ -104,11 +85,14 @@ test('ApiKeyLink', async () => {
   });
 
   expect(responseHandler).toHaveBeenCalledTimes(1);
-  let [{ url, method, headers }] = callArgs(responseHandler);
-  expect(method).toBe('GET');
-  expect(url.href).toBe('http://bugzilla.test.org/test/rest/foo');
-  expect(headers.get('Accept')).toBe('application/json');
-  expect(headers.get('X-BUGZILLA-API-KEY')).toBe('my-api-key');
+  expect(responseHandler).toHaveBeenCalledWith(
+    expect.objectContaining({
+      request: expect.objectContaining({
+        method: 'GET',
+        url: 'http://bugzilla.test.org/test/rest/foo',
+      }),
+    }),
+  );
 });
 
 test('PasswordLink', async () => {
@@ -119,30 +103,32 @@ test('PasswordLink', async () => {
     true,
   );
 
-  let loginHandler = jest.fn((req, res, ctx) =>
-    res(
-      ctx.json({
-        id: 57,
-        token: 'my-token',
-      }),
-    ),
+  let loginHandler = jest.fn(
+    () =>
+      new HttpResponse(
+        JSON.stringify({
+          id: 57,
+          token: 'my-token',
+        }),
+      ),
   );
 
   server.use(
-    rest.get('http://bugzilla.test.org/test/rest/login', loginHandler),
+    http.get('http://bugzilla.test.org/test/rest/login', loginHandler),
   );
 
-  let responseHandler = jest.fn((req, res, ctx) =>
-    res(
-      ctx.json({
-        foo: 'Bar',
-        length: 38,
-      }),
-    ),
+  let responseHandler = jest.fn(
+    () =>
+      new HttpResponse(
+        JSON.stringify({
+          foo: 'Bar',
+          length: 38,
+        }),
+      ),
   );
 
   server.use(
-    rest.get('http://bugzilla.test.org/test/rest/foo', responseHandler),
+    http.get('http://bugzilla.test.org/test/rest/foo', responseHandler),
   );
 
   let testSpec = object({
@@ -158,18 +144,24 @@ test('PasswordLink', async () => {
   });
 
   expect(loginHandler).toHaveBeenCalledTimes(1);
-  let [{ url, method, headers }] = callArgs(loginHandler);
-  expect(method).toBe('GET');
-  expect(url.href).toBe(
-    'http://bugzilla.test.org/test/rest/login?login=my-name&password=my-password&restrict_login=true',
+  expect(loginHandler).toHaveBeenCalledWith(
+    expect.objectContaining({
+      request: expect.objectContaining({
+        method: 'GET',
+        url: 'http://bugzilla.test.org/test/rest/login?login=my-name&password=my-password&restrict_login=true',
+      }),
+    }),
   );
 
   expect(responseHandler).toHaveBeenCalledTimes(1);
-  [{ url, method, headers }] = callArgs(responseHandler);
-  expect(method).toBe('GET');
-  expect(url.href).toBe('http://bugzilla.test.org/test/rest/foo');
-  expect(headers.get('Accept')).toBe('application/json');
-  expect(headers.get('X-BUGZILLA-TOKEN')).toBe('my-token');
+  expect(responseHandler).toHaveBeenCalledWith(
+    expect.objectContaining({
+      request: expect.objectContaining({
+        method: 'GET',
+        url: 'http://bugzilla.test.org/test/rest/foo',
+      }),
+    }),
+  );
 
   loginHandler.mockClear();
   responseHandler.mockClear();
@@ -178,9 +170,12 @@ test('PasswordLink', async () => {
   expect(loginHandler).toHaveBeenCalledTimes(0);
 
   expect(responseHandler).toHaveBeenCalledTimes(1);
-  [{ url, method, headers }] = callArgs(responseHandler);
-  expect(method).toBe('GET');
-  expect(url.href).toBe('http://bugzilla.test.org/test/rest/foo');
-  expect(headers.get('Accept')).toBe('application/json');
-  expect(headers.get('X-BUGZILLA-TOKEN')).toBe('my-token');
+  expect(responseHandler).toHaveBeenCalledWith(
+    expect.objectContaining({
+      request: expect.objectContaining({
+        method: 'GET',
+        url: 'http://bugzilla.test.org/test/rest/foo',
+      }),
+    }),
+  );
 });
